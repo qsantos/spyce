dab1b80342769935ca562ea7a88b984f63cd345b
Quentin SANTOS <quentin.santos@worldonline.fr>
2014-08-22 22:22:09 +0200

    Added physical constants

---
+++ constants.py
@@ -0,0 +1,99 @@
+"""
+Main physical constants
+"""
+
+# universal constants
+
+# speed of ligth (m/s)
+c = 299792458
+
+# gravitational constant (N.(m/kg)^2)
+# F = G mM / r^2
+G = 6.67384e-11
+
+# Planck constant (J.s)
+h = 6.6260695729e-34
+
+
+
+# electromagnetic constants
+
+# magnetic permeability in vacuum (H/m)
+mu0 = 1.2566370614e-6
+
+# elementary charge (C)
+e = 1.60217656635e-19
+
+
+
+# nuclear constatns
+
+# electron mass (kg)
+m_e = 9.1093829140e-31
+
+# proton mass (kg)
+m_p = 1.67262177774e-27
+
+
+
+# physico-chemical constants
+
+# atomic mass constant (kg)
+m_u = 1.66053892173e-27
+
+# Avogadro constant (1/mol)
+N_A = 6.0221412927e23
+
+# Bolzmann constant (J/K)
+k = 1.380648813e-23
+
+# gas constant (J/mol/K)
+# PV = nRT
+R = 8.31447
+
+
+
+# adopted values
+
+# standard gravity (m/s^2)
+# a = m * g0
+g0 = 9.80665
+
+# standard temperature and pressure (K, Pa)
+T0 = 288.15
+p0 = 101325e3
+atm = p0
+
+
+
+# natural units
+
+# Planck length (m)
+l_P = 1.61619997e-35
+
+# Planck mass (kg)
+m_P = 2.1765113e-8
+
+# Planck time (s)
+t_P = 5.3910632e-44
+
+# Planck charge (C)
+q_P = 1.87554595641e-18
+
+# Planck temperature (K)
+T_P = 1.41683385e32
+
+
+
+# miscellaneous
+
+# molar mass of air (kg/mol)
+# rho_air = M_air V_air
+M_air = 0.0289644
+
+# temperature lapse rate (K/m)
+# T = T0 - Lz
+L = 0.0064
+
+# light year (m)
+ly = 365*24*60*60*c
