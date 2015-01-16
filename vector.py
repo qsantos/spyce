38eb256f5cc04f06d81c92cfaba711382aebf3b8
Quentin SANTOS <quentin.santos@worldonline.fr>
2015-01-16 20:39:08 +0100

    Slight reduction of floating point rounding errors

--- vector.py
+++ vector.py
@@ -13,13 +13,12 @@ def dot(u, v):
     """Dot product of vectors"""
     u1, u2, u3 = u
     v1, v2, v3 = v
-    return u1*v1 + u2*v2 + u3*v3
+    return math.fsum([u1*v1, u2*v2, u3*v3])
 
 
 def norm(u):
     """Norm of a vector"""
-    u1, u2, u3 = u
-    return math.sqrt(u1*u1 + u2*u2 + u3*u3)
+    return math.sqrt(dot(u, u))
 
 
 def cross(u, v):
