7dc138e8e855fc3f85d593c7b0e8e8a779070e80
Quentin Santos <quentin.santos@worldonline.fr>
2015-06-05 16:49:02 +0200

    Simplify `cfg.py`

--- cfg.py
+++ cfg.py
@@ -1,6 +1,9 @@
 """Parsing utilities for Kerbal Space Program's configuration files"""
 
-import re
+
+def unpack(a, *b):
+    """Short work-around for Python 2 lack of support of a, *b = ..."""
+    return a, b
 
 
 def parse(f):
@@ -10,72 +13,54 @@ def parse(f):
     as a Python object.
     """
     r = {}
-    while True:
-        line = f.readline()
+    for line in f:
+        line, _ = unpack(*line.split("//", 1))  # strip comments
+        line = line.strip()
+
+        if line == "}":  # end of block
+            break
+
         if not line:
-            break
-        s = re.split("(\w+|//|[{}=])", line.strip())
-
-        if len(s) <= 1:
             continue
 
-        key = s[1]
-        if key == "" or key == "//":
-            continue
-        if key == "}":
-            break
+        if "=" in line:  # assignment
+            key, value = line.split("=", 1)
+            key, value = key.strip(), value.strip()
+        else:  # start of block
+            key, end = unpack(*line.split(None, 1))
 
-        if len(s) > 5 and s[3] == "=":
-            val = "".join(s[5:])
-        elif (len(s) >= 4 and s[3] == "{") or f.readline().strip() == "{":
-            val = parse(f)
-        else:
-            s = re.split("\W", line)
-            for key in s:
-                if key != "":
-                    r[key] = True
-            continue
+            if end == ["{"] or next(f).strip() == "{":
+                value = parse(f)  # parse recursively
+            else:
+                raise SyntaxError("Expected '{'")
 
+        # save key/value
         if key in r:
             p = r[key]
             if not isinstance(p, list):
                 r[key] = [p]
-            r[key].append(val)
+            r[key].append(value)
         else:
-            r[key] = val
+            r[key] = value
 
     return r
 
 
-def dic_get_float(dic, key):
-    """Get float a a given key (default: 0.)"""
-    if key in dic:
-        return float(dic[key])
-    else:
-        return 0.
-
-
-def dic_get_string(dic, key):
-    """Get string at a given key (default: "")"""
-    if key in dic:
-        return dic[key]
-    else:
-        return ""
-
-
 def dic_get_group(dic, group, name):
-    """Get element of a given name in a given group.
+    """Get element of a given name in a given group
 
-    For example, find the MODULE named ModuleEngine in the
-    dictionary. If none is found, None is returned.
+    For example, find the MODULE named ModuleEngine.
     """
-    if group not in dic:
+
+    try:
+        g = dic[group]
+    except KeyError:
         return None
-    g = dic[group]
+
     if isinstance(g, list):
-        for _, v in enumerate(g):
-            if "name" in v and v["name"] == name:
+        for v in g:
+            if v.get("name") == name:
                 return v
-    elif "name" in g and g["name"] == name:
+    elif g.get("name") == name:
         return g
     return None
