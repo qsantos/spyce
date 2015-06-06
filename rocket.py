ad9f6e7b68f098e1a247b65e0aa41aea6ec8f57f
Quentin Santos <quentin.santos@worldonline.fr>
2015-06-06 15:32:04 +0200

    Add Rocket class
    `Rocket.simulate()` runs a simplistic simulatiob of a rocket during a given
    time interval. Mass of propellant is ignored.

--- rocket.py
+++ rocket.py
@@ -1,3 +1,4 @@
+import vector
 import physics
 
 
@@ -39,3 +40,63 @@ class RocketPart:
 
     def make_tank(self, propel):
         self.propel = propel
+
+
+class Rocket:
+    def __init__(self):
+        self.parts = set()
+        self.mass = 0.
+
+        self.position = vector.Vector([0, 0, 0])
+        self.velocity = vector.Vector([0, 0, 0])
+        self.acceleration = vector.Vector([0, 0, 0])
+
+        self.orientation = vector.Matrix.identity()
+        self.rotate_deg(90, 0, 1, 0)
+
+    def simulate(self, dt):
+        # propulsion
+        self.acceleration = self.prograde * (self.thrust/self.mass)
+
+        # update velocity and position
+        self.velocity += self.acceleration * dt
+        self.position += self.velocity * dt
+
+    def update_parts(self):
+        self.mass = sum(part.mass for part in self.parts)
+        self.thrust = sum(part.thrust for part in self.parts)
+        self.expulsion_rate = sum(part.expulsion_rate for part in self.parts)
+
+    def __ior__(self, parts):
+        self.parts |= parts
+        self.update_parts()
+        return self
+
+    def __isub__(self, parts):
+        self.parts -= parts
+        self.update_parts()
+        return self
+
+    def rotate_deg(self, angle, x, y, z):
+        self.orientation *= vector.Matrix.rotation_deg(angle, x, y, z)
+        self.prograde = self.orientation * [0, 0, 1]
+
+
+if __name__ == "__main__":
+    import copy
+
+    import ksp_cfg
+    parts = ksp_cfg.get_parts()
+
+    def make_parts(*names):
+        return {copy.copy(parts[name]) for name in names}
+
+    rocket = Rocket()
+
+    stage = make_parts('probeCoreOcto', 'fuelTankSmallFlat', 'liquidEngine3')
+    rocket |= stage
+
+    print(rocket.position[0], rocket.velocity[0], rocket.acceleration[0])
+    for _ in range(10):
+        rocket.simulate(1)
+        print(rocket.position[0], rocket.velocity[0], rocket.acceleration[0])
