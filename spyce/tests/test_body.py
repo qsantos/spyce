1bdbe13a3e9682e4b0bed54eaf28d60c75de5108
Quentin Santos <quentin.santos@worldonline.fr>
2016-02-02 20:08:41 +0100

    More tests for module body

--- spyce/tests/test_body.py
+++ spyce/tests/test_body.py
@@ -1,12 +1,23 @@
 import unittest
 
+import math
 import random
+import physics
 
 import orbit
 import body
+from load import solar
 
 
 class TestBody(unittest.TestCase):
+    def test_gravity(self):
+        Earth = solar['Earth']
+        self.assertAlmostEqual(Earth.gravity(), physics.g0, places=1)
+
+        self.assertEqual(Earth.gravity(0), 0)
+
+        self.assertEqual(Earth.gravity(Earth.radius / 2), Earth.gravity() / 2)
+
     def test_local_time(self):
         star = body.CelestialBody("star", 1e20)
         planet = body.CelestialBody(
@@ -18,6 +29,33 @@ class TestBody(unittest.TestCase):
         time = random.randint(1e6, 1e8)
         local_time = planet.time2str(time)
         self.assertAlmostEqual(planet.str2time(local_time), time, places=0)
+
+        time = -random.randint(1e6, 1e8)
+        local_time = planet.time2str(time)
+        self.assertAlmostEqual(planet.str2time(local_time), time, places=0)
+
+    def test_escape_velocity(self):
+        Earth = solar['Earth']
+        escape_velocity = Earth.escape_velocity(Earth.radius)
+        self.assertAlmostEqual(escape_velocity, 11186, places=0)
+
+    def test_angular_diameter(self):
+        Sun = solar['Sun']
+        Earth = solar['Earth']
+        Moon = solar['Moon']
+
+        computed = Sun.angular_diameter(Earth.orbit.periapsis)
+        expected = math.radians(32./60 + 32./3600)
+        self.assertAlmostEqual(computed, expected, places=5)
+
+        computed = Moon.angular_diameter(Moon.orbit.apoapsis)
+        expected = math.radians(29./60 + 26./3600)
+        self.assertAlmostEqual(computed, expected, places=5)
+
+    def test_name(self):
+        Earth = solar['Earth']
+        self.assertEqual(str(Earth), 'Earth')
+        self.assertEqual(repr(Earth), '<Earth>')
 
 
 if __name__ == '__main__':
