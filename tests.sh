237e2582a51c4811c2040313323b40c98dac0465
Quentin Santos <quentin.santos@worldonline.fr>
2016-02-02 07:51:13 +0100

    Check test coverage of all source files

--- tests.sh
+++ tests.sh
@@ -5,11 +5,11 @@ cd $(dirname $0)
 cd spyce/
 
 echo "Running test for python2"
-python2-coverage run -m unittest discover
+python2-coverage run --source . -m unittest discover
 python2-coverage report
 
 echo "Running test for python3"
-python3-coverage run -m unittest discover
+python3-coverage run --source . -m unittest discover
 python3-coverage report
 
 cd ..
