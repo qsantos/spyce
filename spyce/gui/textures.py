8330bb35b93cdd076408765451a2515a9a35179a
Quentin Santos <quentin.santos@worldonline.fr>
2016-07-06 02:01:09 +0200

    Enable anti-aliasing

--- spyce/gui/textures.py
+++ spyce/gui/textures.py
@@ -69,8 +69,9 @@ else:
         glTexImage2D(
             GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data
         )
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        # anti-aliasing
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         glBindTexture(GL_TEXTURE_2D, 0)
 
         return new_tex
