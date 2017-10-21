eed7dce5c07d9feb0b6a8ebefb77d7afee1047b1
Quentin Santos <quentin.santos@worldonline.fr>
2017-10-21 10:33:08 +0200

    gspyce: function to load cubemap textures

--- gspyce/textures.py
+++ gspyce/textures.py
@@ -42,27 +42,33 @@ except ImportError:
     def load(*_, **__):
         """No texture can be loaded with PIL; return dummy texture 0"""
         return 0
-else:
-    def load(*path):
-        """Make texture from file at given path (join arguments)
 
-        If loading the file fails, return dummy texture 0"""
+    def load_cubemap(*path):
+        return 0
+else:
+    def load_image(*path):
+        """Return raw picels from file at given path (join arguments)"""
         filename = os.path.join("data", "textures", *path)
         try:
             image = pkgutil.get_data("gspyce", filename)
         except FileNotFoundError:
             print("Missing %s" % filename, file=sys.stderr)
-            return 0
+            return None, None, None
 
         im = Image.open(io.BytesIO(image))
         w, h = im.size
-        try:
-            data = im.tobytes("raw", "RGBA", 0, -1)
-        except (SystemError, ValueError):
-            try:
-                data = im.tobytes("raw", "RGBX", 0, -1)
-            except (SystemError, ValueError):
-                data = im.convert("RGBA").tobytes("raw", "RGBA", 0, -1)
+        # <https://pillow.readthedocs.io/en/4.3.x/handbook/writing-your-own-file-decoder.html#the-raw-decoder>
+        # orientation=-1 ⇒ first row at bottom (as OpenGL expects)
+        data = im.convert("RGBA").tobytes("raw", "RGBA", 0, -1)
+        return w, h, data
+
+    def load(*path):
+        """Make texture from file at given path (join arguments)
+
+        If loading the file fails, return dummy texture 0"""
+        w, h, data = load_image(*path)
+        if data is None:
+            return 0
 
         new_tex = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, new_tex)
@@ -74,3 +80,29 @@ else:
         glBindTexture(GL_TEXTURE_2D, 0)
 
         return new_tex
+
+    def load_cubemap(*path):
+        """Create a cubemap
+
+        Arguments are joined together to make the paths to the textures. Last
+        argument should be a pattern with a "%s", which will be completed as
+        "PositiveX", "NegativeY" and so on."""
+
+        texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texture)
+        path, file_pattern = list(path[:-1]), path[-1]
+        faces = ["PositiveX", "NegativeX", "PositiveY", "NegativeY", "PositiveZ", "NegativeZ"]
+        for i, face in enumerate(faces):
+            full_path = path + [file_pattern % face]
+            w, h, data = load_image(*full_path)
+            if data is None:
+                continue
+            glTexImage2D(
+                GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data
+            )
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glBindTexture(GL_TEXTURE_CUBE_MAP, 0)
+
+        return texture
