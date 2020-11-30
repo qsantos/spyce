66497b61e52b835487c978490a4fc1ae619d6e91
Valery Arugin <iamarugin@gmail.com>
2020-11-30 16:04:27 +0300

    Add more dwarf planets

--- get_nasa_data.py
+++ get_nasa_data.py
@@ -358,7 +358,7 @@ def main():
     get_moons_orbits(bodies)
 
     print('Loading dwarf planets data')
-    dwarf_planets = ['Ceres', 'Pluto', 'Sedna', 'Haumea', 'Makemake', 'Eris']
+    dwarf_planets = ['Ceres', 'Pluto', 'Sedna', 'Haumea', 'Makemake', 'Eris', 'Orcus', 'Quaoar', 'Gonggong']
     for dwarf_planet in dwarf_planets:
         get_dwarf_planet_data(bodies, dwarf_planet)
 
